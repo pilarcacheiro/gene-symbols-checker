f5c8fe2fefe380d838257c3ec67b8166cd4da227
Pilar Cacheiro <pilarcacheiro@gmail.com>
2023-04-13 16:08:56 +0100

    tidy script , include log file

--- hgnc_symbol_checker.R
+++ hgnc_symbol_checker.R
@@ -13,6 +13,18 @@ library("dplyr")
 if (!require("tidyr")) install.packages("tidyr")
 library("tidyr")
 
+if (!require("logr")) install.packages("logr")
+library("logr")
+
+# create log file ---------------------------------------------------------
+
+## option to generate a log file to have a record of the 
+## date the HGNC file was accessed through the FTP
+## create a directory for log files
+
+tmp <- file.path("./data/logs", "hgnc_checker.log")
+
+lf <- log_open(tmp)
 
 # hgnc gene file ----------------------------------------------------------
 
@@ -24,7 +36,7 @@ if (!file.exists(filename)) {
                     sep = "/")
 }
 
-protein.coding.genes <- readr::read_delim(filename,
+protein_coding_genes <- readr::read_delim(filename,
                                           delim = "\t",
                                           col_names = TRUE)
 
@@ -43,7 +55,7 @@ protein.coding.genes <- readr::read_delim(filename,
 # hgnc checker function ---------------------------------------------------
 
 
-hgnc.checker <- function(gene.symbols, gene.file) {
+hgnc.checker <- function(gene_symbols, gene_file) {
   ## Purpose:
   ##   check gene symbols and retrieve hgnc id (stable identifiers)
   ##
@@ -52,117 +64,115 @@ hgnc.checker <- function(gene.symbols, gene.file) {
   ##   however, the input file could be modified to include other genes
   ##
   ## Input:
-  ##   gene.symbols: a vector of gene symbols to be checked
+  ##   gene_symbols: a vector of gene symbols to be checked
   ##   gene.file: a data frame 'gene_with_protein_product.txt' (see README)
   ##              file from the hgnc website
   ##
   ## Output:
   ##   A dataframe with 3 columns:
-  ##     c1) 'HGNC.ID': corresponding hgnc id ('-' , if no hgnc id was found)
-  ##     c2) 'Gene.Symbol': gene symbol provided
-  ##     c3) 'Type': mapping type (Approved.Symbol,
-  ##                               Synonym.Symbol,
-  ##                               Notfound.ProteinCoding.Symbol,...)
+  ##     c1) 'hgnc_id': corresponding hgnc id ('-' , if no hgnc id was found)
+  ##     c2) 'gene_symbol': gene symbol provided
+  ##     c3) 'type': mapping type (approved_symbol,
+  ##                               synonym_symbol,
+  ##                               notfound_proteinpoding_symbol,...)
   
   
-  check.approved <- function(input.genes, database) {
+  check.approved <- function(input_genes, database) {
     ## A function to check if the input matches an approved gene symbol
     
     return(database %>%
-             select(hgnc_id, symbol) %>%
+             dplyr::select(hgnc_id, symbol) %>%
              mutate_if(is.factor, as.character) %>%
              filter(symbol != "") %>%
              filter(!is.na(symbol)) %>%
              filter(symbol %in%
-                      input.genes) %>%
-             rename(Gene.Symbol = symbol, HGNC.ID = hgnc_id) %>%
-             mutate(Type = "Approved.Symbol"))
+                      input_genes) %>%
+             dplyr::rename(gene_symbol = symbol) %>%
+             mutate(type = "approved_symbol"))
   }
   
   
-  check.synonyms <- function(input.genes, database) {
+  check.synonyms <- function(input_genes, database) {
     ## A function to check if the input symbol corresponds to an
     ## alias / synonym gene symbol
     
     return(database %>%
-             select(hgnc_id, alias_symbol) %>%
+             dplyr::select(hgnc_id, alias_symbol) %>%
              mutate_if(is.factor,
                        as.character) %>%
              filter(alias_symbol != "") %>%
              filter(!is.na(alias_symbol)) %>%
              separate_rows(alias_symbol, sep = "\\|") %>%
-             rename(HGNC.ID = hgnc_id) %>%
-             mutate(Gene.Symbol = trimws(alias_symbol)) %>%
-             select(HGNC.ID, Gene.Symbol) %>%
-             filter(Gene.Symbol %in% input.genes) %>%
-             mutate(Type = "Synonym.Symbol"))
+             mutate(gene_symbol = trimws(alias_symbol)) %>%
+             dplyr::select(hgnc_id, gene_symbol) %>%
+             filter(gene_symbol %in% input_genes) %>%
+             mutate(type = "synonym_symbol"))
   }
   
   
-  check.previous <- function(input.genes, database) {
+  check.previous <- function(input_genes, database) {
     ## A function to check if the input symbol corresponds to a previous
     ## official gene symbol
     
     return(database %>%
-             select(hgnc_id, prev_symbol) %>%
+             dplyr::select(hgnc_id, prev_symbol) %>%
              mutate_if(is.factor,
                        as.character) %>%
              filter(prev_symbol != "") %>%
              filter(!is.na(prev_symbol)) %>%
              separate_rows(prev_symbol, sep = "\\|") %>%
-             rename(HGNC.ID = hgnc_id) %>%
-             mutate(Gene.Symbol = trimws(prev_symbol)) %>%
-             dplyr::select(HGNC.ID,
-                           Gene.Symbol) %>%
-             filter(Gene.Symbol %in% input.genes) %>%
-             mutate(Type = "Previous.Symbol"))
+             mutate(gene_symbol = trimws(prev_symbol)) %>%
+             dplyr::select(hgnc_id,
+                           gene_symbol) %>%
+             filter(gene_symbol %in% input_genes) %>%
+             mutate(type = "previous_symbol"))
   }
   
   
-  check.duplicates.symbol <- function(file.to.check.symbols,
-                                      duplicates.symbol) {
+  check.duplicates.symbol <- function(file_to_check_symbols,
+                                      duplicates_symbol) {
     ## A function to check if we have any duplicated symbols
     
-    if (!length(duplicates.symbol)) {
-      return(file.to.check.symbols)
+    if (!length(duplicates_symbol)) {
+      return(file_to_check_symbols)
       
     } else {
       
-      final.nodup.symbol <- file.to.check.symbols %>%
-        filter(!Gene.Symbol %in%
-                 duplicates.symbol)
+      final_nodup_symbol <- file_to_check_symbols %>%
+        filter(!gene_symbol %in%
+                 duplicates_symbol)
       
-      duplicate.symbols.df <- data.frame(HGNC.ID = rep("-",
-                                                       length(duplicates.symbol)),
-                                         Gene.Symbol = duplicates.symbol,
-                                         Type = "Ambiguous.Symbol")
+      duplicate_symbols_df <- data.frame(hgnc_id = rep("-",
+                                                       length(duplicates_symbol)),
+                                         gene_symbol = duplicates_symbol,
+                                         type = "ambiguous_symbol")
       
-      final.dups.symbol <- rbind(final.nodup.symbol, duplicate.symbols.df)
+      final_dups_symbol <- rbind(final_nodup_symbol, duplicate_symbols_df)
       
-      return(final.dups.symbol)
+      return(final_dups_symbol)
       
     }
   }
   
   
-  check.duplicates.id <- function(file.to.check.ids, duplicates.id) {
+  check.duplicates.id <- function(file_to_check_ids, duplicates_id) {
     ## A function to check if we have any duplicated hgnc ids in the
     ## resulting file
     
-    if (!length(duplicates.id)) {
-      return(file.to.check.ids)
+    if (!length(duplicates_id)) {
+      return(file_to_check_ids)
     } else {
       
-      final.nodup.id <- file.to.check.ids %>%
-        filter(!HGNC.ID %in% duplicates.id)
+      final_nodup_id <- file_to_check_ids %>%
+        filter(!hgnc_id %in% duplicates_id)
       
-      duplicate.ids.df <- file.to.check.ids %>%
-        filter(HGNC.ID %in% duplicates.id) %>%
-        mutate(HGNC.ID = "-", Type = "Ambiguous.Symbol")
+      duplicate_ids_df <- file_to_check_ids %>%
+        filter(hgnc_id %in% duplicates_id) %>%
+        mutate(hgnc_id = "-", type = "ambiguous_symbol")
       
-      final.dups.id <- rbind(final.nodup.id, duplicate.ids.df)
+      final_dups_id <- rbind(final_nodup_id, duplicate_ids_df)
       
-      return(final.dups.id)
+      return(final_dups_id)
       
     }
   }
@@ -170,76 +180,83 @@ hgnc.checker <- function(gene.symbols, gene.file) {
   # we make sure we remove any leading or trailing white space in our vector
   # with gene names
   
-  genes <- trimws(gene.symbols)
+  genes <- trimws(gene_symbols)
   
-  hgnc <- gene.file
+  hgnc <- gene_file
   
   # we first check with gene symbols match the official gene symbol
   
-  hgnc.approved <- check.approved(genes, hgnc)
+  hgnc_approved <- check.approved(genes, hgnc)
   
   
   # we next check with gene symbols not matching the official gene symbols are
   # synonyms or alias of an approved symbol
   
-  hgnc.synonyms <- check.synonyms(genes[!genes %in% hgnc.approved$Gene.Symbol],
+  hgnc_synonyms <- check.synonyms(genes[!genes %in% hgnc_approved$gene_symbol],
                                   hgnc)
   
   
   # we next check with gene symbols not matching the official gene symbols are
   # synonyms or alias of an approved symbol
   
-  hgnc.previous <- check.previous(genes[!genes %in% c(hgnc.approved$Gene.Symbol,
-                                                      hgnc.synonyms$Gene.Symbol)],
+  hgnc_previous <- check.previous(genes[!genes %in% c(hgnc_approved$gene_symbol,
+                                                      hgnc_synonyms$gene_symbol)],
                                   hgnc)
   
   
   # we next identified with genes have not been identified in the hgnc file
   # and create a data frame with 3 columns
   
-  genes.not.found <- genes[!genes %in% c(hgnc.approved$Gene.Symbol,
-                                         hgnc.synonyms$Gene.Symbol,
-                                         hgnc.previous$Gene.Symbol)]
+  genes_not_found <- genes[!genes %in% c(hgnc_approved$gene_symbol,
+                                         hgnc_synonyms$gene_symbol,
+                                         hgnc_previous$gene_symbol)]
   
   
-  hgnc.notfound <- data.frame(HGNC.ID = rep("-", length(genes.not.found)),
-                              Gene.Symbol = genes.not.found) %>%
-    mutate(Type = "Notfound.ProteinCoding.Symbol") %>%
+  hgnc_notfound <- data.frame(hgnc_id = rep("-", length(genes_not_found)),
+                              gene_symbol = genes_not_found) %>%
+    mutate(type = "notfound_proteincoding_symbol") %>%
     mutate_if(is.factor, as.character)
   
   
   # we bind all the previous data frames
   
-  hgnc.all <- hgnc.approved %>%
-    bind_rows(hgnc.synonyms) %>%
-    bind_rows(hgnc.previous) %>%
-    bind_rows(hgnc.notfound)
+  hgnc_all <- hgnc_approved %>%
+    bind_rows(hgnc_synonyms) %>%
+    bind_rows(hgnc_previous) %>%
+    bind_rows(hgnc_notfound)
   
   
   # we look for any potential duplicated gene symbols or hgnc ids in our
   # dataset
   
-  duplicates.symbol <- hgnc.all %>%
-    filter(duplicated(Gene.Symbol)) %>%
-    pull(Gene.Symbol)
+  duplicates_symbol <- hgnc_all %>%
+    filter(duplicated(gene_symbol)) %>%
+    pull(gene_symbol)
   
   
-  results.noduplicated.symbol <- check.duplicates.symbol(hgnc.all,
-                                                         duplicates.symbol)
+  results_noduplicated_symbol <- check.duplicates.symbol(hgnc_all,
+                                                         duplicates_symbol)
   
   
-  duplicates.id <- results.noduplicated.symbol %>%
-    filter(duplicated(HGNC.ID)) %>%
-    filter(HGNC.ID != "-") %>%
-    pull(HGNC.ID)
+  duplicates_id <- results_noduplicated_symbol %>%
+    filter(duplicated(hgnc_id)) %>%
+    filter(hgnc_id != "-") %>%
+    pull(hgnc_id)
   
-  results.noduplicated.id <- check.duplicates.id(results.noduplicated.symbol,
-                                                 duplicates.id)
+  results_noduplicated_id <- check.duplicates.id(results_noduplicated_symbol,
+                                                 duplicates_id)
   
   
   # final dataframe
   
-  results.final <- results.noduplicated.id
+  results_final <- results_noduplicated_id
   
-  return(results.final)
+  return(results_final)
+  
+  # log file ----------------------------------------------------------------
+  
+  
+  log_print("hgnc checker")
+  
+  log_close()
 }
